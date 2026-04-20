6954c7124d54802740cec13c559df4f8702e07e6
Duongnt <duongtungnguyen88@gmail.com>
2026-04-20 09:21:22 +0700

    ft: cloudinary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
+
+    // HTTP client for Cloudinary upload API
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
